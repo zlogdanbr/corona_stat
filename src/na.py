9f9839f4b5412258452905364be92e82138419ba
zlogdanbr <54685402+zlogdanbr@users.noreply.github.com>
2020-04-29 11:13:52 -0300

    Update na.py

--- src/na.py
+++ src/na.py
@@ -1,3 +1,42 @@
+'''
+Description: Functions to calculate and plot polinomial regressions
+for coronavirus cases
+
+author: daniel.vasconcelosgomes at gmail com
+29/04/2020
+
+Usage:
+
+python na.py CSV_FILE Title Days_since_start
+eg:
+
+> python na.py sp.csv SP 57
+
+
+[Polinomial regression level: 8 ]
+RMSE: 282.76527302319323
+R^2:  0.9983290353285625
+Maximum cases ( SP ) estimated: 143816.35359523178 at 79 days.
+Time to double: [7.29528157] days
+
+
+References and sources:
+
+https://www.python-course.eu/numerical_programming_with_python.php
+https://www.analyticsvidhya.com/blog/2020/03/polynomial-regression-python/
+https://towardsdatascience.com/polynomial-regression-bbe8b9d97491
+https://www.math.ust.hk/~machas/mathematical-biology.pdf
+https://www.researchgate.net/publication/305032108_The_mathematical_modelling_of_population_change
+https://covid.saude.gov.br/
+https://www.seade.gov.br/coronavirus/
+http://coronavirus.dadosecausas.org/
+https://ourworldindata.org/coronavirus
+https://docs.google.com/spreadsheets/d/1gR1QaUdWCclJxE4q1huY-SGc4NxbWjeVUscSmvMO8d4/edit?usp=sharing
+
+
+
+'''
+
 import pandas as pd
 import numpy as np
 
@@ -89,6 +128,8 @@ def show_error( y, y_poly_pred ):
     print( "RMSE: {}".format(rmse))
     print( "R^2:  {}".format(r2))
 
+
+
 '''
 Based on two data sets plots the graphic yxt
 using title and labels for reference
@@ -139,6 +180,10 @@ def do_pol_reg( t, y, msg, level, today  ):
     #print_regression( t, y, pol_reg, poly_reg, "Cases Corona", "days", "Cases" )    
     
 
+'''
+Based on two data sets yxt calculates the best polinomial that gives
+the shortest RMSE
+'''
 def find_min_error( t, y ):
     
     best_fit = []
@@ -171,10 +216,10 @@ def main(argv):
     n = find_min_error ( t, y )  
     do_pol_reg( t, y, option , n, today )
 
-
                          
-        
+# main program start  ---------------------------------------------    
                  
 if __name__ == '__main__':
     main(sys.argv)  
-    
+
+# main program end  -----------------------------------------------       
